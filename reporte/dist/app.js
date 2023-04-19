874242a8d69be11703786b5d8f0051008423c50d
Nicolas <nhidalgo@cyd-tec.com>
2023-04-19 16:41:13 -0400

    update main.yml and app.js

--- reporte/dist/app.js
+++ reporte/dist/app.js
@@ -4,6 +4,7 @@ var dotenv = _interopRequireWildcard(require("dotenv"));
 var _path = _interopRequireDefault(require("path"));
 var _express = _interopRequireDefault(require("express"));
 var _bodyParser = _interopRequireDefault(require("body-parser"));
+var _cors = _interopRequireDefault(require("cors"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -11,6 +12,7 @@ dotenv.config({
   path: _path.default.join(__dirname, "..", ".env")
 });
 const app = (0, _express.default)();
+app.use((0, _cors.default)());
 app.use(_bodyParser.default.urlencoded({
   extended: true
 }));
@@ -20,7 +22,7 @@ app.use(_express.default.json({
 const port = process.env.PORT || 5001;
 app.get("/", (req, res) => {
   return res.json({
-    msg: "ok",
+    msg: "Cypress test",
     status: 200
   });
 });
